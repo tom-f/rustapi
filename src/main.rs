29ef3b25ac22f1bc8d401b16dba26f062194cb5a
Tom Fawssett <tom.fawssett@gmail.com>
2022-03-26 13:07:15 +0000

    main style refactor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
+use std::net::SocketAddr;
+use rustapi::app;
+
 #[tokio::main]
 async fn main() {
     // run it
-    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], 3000));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
-        .serve(rustapi::app().into_make_service())
+        .serve(app().into_make_service())
         .await
         .unwrap();
 }
