f2af463256e2f679541351bcf28a92d6b58ce90c
Tom Fawssett <tom.fawssett@gmail.com>
2022-06-22 16:39:49 +0100

    commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,40 @@
-use axum::{http::StatusCode, routing::get, Router};
+use std::fmt::Debug;
+
+use axum::{
+    extract::{rejection::FormRejection, Form},
+    http::StatusCode,
+    routing::get,
+    routing::post,
+    Router,
+};
+
+use serde::Deserialize;
 
 pub fn app() -> Router {
-    Router::new().route("/health-check", get(health_check))
+    Router::new()
+        .route("/health-check", get(health_check))
+        .route("/subscribe", post(subscribe))
 }
 
 async fn health_check() -> StatusCode {
     StatusCode::OK
+}
+
+#[derive(Debug, Deserialize)]
+#[allow(dead_code)]
+struct SubscribeForm {
+    name: String,
+    email: String,
+}
+
+async fn subscribe(input: Result<Form<SubscribeForm>, FormRejection>) -> (StatusCode, String) {
+    match input {
+        Ok(_) => (StatusCode::OK, "".to_string()),
+        Err(rejection) => match rejection {
+            FormRejection::FailedToDeserializeQueryString { .. } => {
+                (StatusCode::BAD_REQUEST, format! {"{:?}", rejection})
+            }
+            _ => (StatusCode::INTERNAL_SERVER_ERROR, "".to_string()),
+        },
+    }
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -24,3 +24,56 @@ async fn health_check_works() {
     let body = hyper::body::to_bytes(response.into_body()).await.unwrap();
     assert_eq!(body.to_vec().len(), 0);
 }
+
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app = app();
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .uri("/subscribe")
+                .method("POST")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .body(Body::from(
+                    "name=le%20guin&email=ursula_le_guin%40gmail.com",
+                ))
+                .unwrap(),
+        )
+        .await
+        .unwrap();
+    // .expect("Failed to execute request.");
+
+    assert_eq!(response.status(), StatusCode::OK);
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_for_invalid_form_data() {
+    let tests = vec![
+        ("name=le%20guin", "missing email field"),
+        ("email=ursula_le_guin%40gmail.com", "missing name field"),
+        ("", "missing all fields"),
+    ];
+
+    for (data, message) in tests {
+        let app = app();
+        let response = app
+            .oneshot(
+                Request::builder()
+                    .uri("/subscribe")
+                    .method("POST")
+                    .header("Content-Type", "application/x-www-form-urlencoded")
+                    .body(Body::from(data))
+                    .unwrap(),
+            )
+            .await
+            .expect("Failed to execute request.");
+
+        assert_eq!(
+            response.status(),
+            StatusCode::BAD_REQUEST,
+            "did not get expected status code when payload was {}",
+            message
+        );
+    }
+}
